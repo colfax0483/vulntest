0054d20baad6d80606cf94e52fed3a0fe289c648
PeanutK <rharnr777@gmail.com>
2024-09-19 20:06:23 +0900

    Create simpleshell.php

---
+++ simpleshell.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <form method="GET" name="simpleshell.php">
+      <input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+      <input type="SUBMIT" value="Execute">
+    </form>
+    <pre>
+    </pre>
+  </body>
+</html>
